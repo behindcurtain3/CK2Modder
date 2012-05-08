d1d2e37e143c6aa1bd7758d5bfe8b181b407061e
Justin <behindcurtain3@gmail.com>
2012-05-08 00:59:02 -0700

    initial commit

---
+++ CK2Modder/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Drawing;
+
+namespace CK2Modder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.workingLocationStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabDynasties = new System.Windows.Forms.TabPage();
+            this.dynastyGridView = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DynastyName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Culture = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabCharacters = new System.Windows.Forms.TabPage();
+            this.dynastyBackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.mainMenuStrip.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.tabControl.SuspendLayout();
+            this.tabDynasties.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dynastyGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(784, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "mainMenuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(109, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar,
+            this.workingLocationStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 540);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // workingLocationStripStatusLabel
+            // 
+            this.workingLocationStripStatusLabel.Name = "workingLocationStripStatusLabel";
+            this.workingLocationStripStatusLabel.Size = new System.Drawing.Size(104, 17);
+            this.workingLocationStripStatusLabel.Text = "Working Location:";
+            // 
+            // toolStripProgressBar
+            // 
+            this.toolStripProgressBar.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripProgressBar.Name = "toolStripProgressBar";
+            this.toolStripProgressBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Controls.Add(this.tabDynasties);
+            this.tabControl.Controls.Add(this.tabCharacters);
+            this.tabControl.Location = new System.Drawing.Point(3, 27);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(781, 510);
+            this.tabControl.TabIndex = 2;
+            // 
+            // tabDynasties
+            // 
+            this.tabDynasties.Controls.Add(this.dynastyGridView);
+            this.tabDynasties.Location = new System.Drawing.Point(4, 22);
+            this.tabDynasties.Name = "tabDynasties";
+            this.tabDynasties.Padding = new System.Windows.Forms.Padding(3);
+            this.tabDynasties.Size = new System.Drawing.Size(773, 484);
+            this.tabDynasties.TabIndex = 0;
+            this.tabDynasties.Text = "Dynasties";
+            this.tabDynasties.UseVisualStyleBackColor = true;
+            // 
+            // dynastyGridView
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Navy;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dynastyGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dynastyGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dynastyGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.DynastyName,
+            this.Culture});
+            this.dynastyGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dynastyGridView.Location = new System.Drawing.Point(3, 3);
+            this.dynastyGridView.Name = "dynastyGridView";
+            this.dynastyGridView.Size = new System.Drawing.Size(767, 478);
+            this.dynastyGridView.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.ToolTipText = "The ID of the Dynasty";
+            // 
+            // DynastyName
+            // 
+            this.DynastyName.HeaderText = "Name";
+            this.DynastyName.Name = "DynastyName";
+            this.DynastyName.ToolTipText = "The Name of the Dynasty";
+            // 
+            // Culture
+            // 
+            this.Culture.HeaderText = "Culture";
+            this.Culture.Name = "Culture";
+            this.Culture.ToolTipText = "The Culture of the Dynasty";
+            // 
+            // tabCharacters
+            // 
+            this.tabCharacters.Location = new System.Drawing.Point(4, 22);
+            this.tabCharacters.Name = "tabCharacters";
+            this.tabCharacters.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCharacters.Size = new System.Drawing.Size(773, 484);
+            this.tabCharacters.TabIndex = 1;
+            this.tabCharacters.Text = "Characters";
+            this.tabCharacters.UseVisualStyleBackColor = true;
+            // 
+            // dynastyBackgroundWorker
+            // 
+            this.dynastyBackgroundWorker.WorkerReportsProgress = true;
+            this.dynastyBackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.dynastyBackgroundWorker_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "Form1";
+            this.Text = "CK2 Modder";
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tabControl.ResumeLayout(false);
+            this.tabDynasties.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dynastyGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel workingLocationStripStatusLabel;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabDynasties;
+        private System.Windows.Forms.TabPage tabCharacters;
+        private System.Windows.Forms.DataGridView dynastyGridView;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DynastyName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Culture;
+        private System.ComponentModel.BackgroundWorker dynastyBackgroundWorker;
+    }
+}
+
---
+++ CK2Modder/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace CK2Modder
+{
+    public partial class Form1 : Form
+    {
+        public static readonly String SteamDirectory = "C:\\Program Files\\Steam\\steamapps\\common\\crusader kings ii";
+        public static readonly String DynastyFile = "/common/dynasties.txt";
+
+        public String SelectedFolder { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            toolStripProgressBar.Visible = false;
+
+            dynastyBackgroundWorker.ProgressChanged += new ProgressChangedEventHandler(dynastyBackgroundWorker_ProgressChanged);
+            dynastyBackgroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(dynastyBackgroundWorker_RunWorkerCompleted);
+        }        
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog = new FolderBrowserDialog();
+            //folderBrowserDialog.RootFolder = Environment.SpecialFolder.MyComputer;
+            folderBrowserDialog.Description = "Select the main Crusader Kings II installation directory.";
+            folderBrowserDialog.SelectedPath = SteamDirectory;
+            folderBrowserDialog.ShowNewFolderButton = false;
+
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                SelectedFolder = folderBrowserDialog.SelectedPath;
+
+                if (!SelectedFolder.Equals(""))
+                {
+                    workingLocationStripStatusLabel.Text = String.Format("Working Location: {0}", SelectedFolder);
+                    LoadDynastiesInfo();
+                }
+            }
+        }
+
+        private void LoadDynastiesInfo()
+        {
+            String absolutePath = SelectedFolder + DynastyFile;
+
+            if (File.Exists(absolutePath))
+            {
+                StreamReader stream = File.OpenText(absolutePath);
+
+                toolStripProgressBar.Visible = true;
+                toolStripProgressBar.Value = 0;
+                toolStripProgressBar.Maximum = 100;
+
+                dynastyBackgroundWorker.RunWorkerAsync(stream);
+            }
+        }
+
+        private void dynastyBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int progress = 0;
+            // Store the ids
+            List<String[]> rows = new List<string[]>();
+
+            BackgroundWorker worker = sender as BackgroundWorker;
+            StreamReader stream = e.Argument as StreamReader;
+
+            String line;
+
+            while ((line = stream.ReadLine()) != null)
+            {
+                // Start of dynasty
+                if (line.EndsWith("= {") && !line.StartsWith("#"))
+                {
+                    String[] row = new String[3];
+                    row[0] = Regex.Match(line, @"\d+").Value;
+                    row[1] = "";
+                    row[2] = "";
+
+                    int openingBrackets = 1;
+                    int closingBrackets = 0;
+
+                    String insideLine;
+
+                    do
+                    {
+                        insideLine = stream.ReadLine();
+
+                        if (insideLine.Contains("{"))
+                            openingBrackets++;
+                        else if (insideLine.Contains("}"))
+                            closingBrackets++;
+
+                        if (insideLine.Contains("name"))
+                        {
+                            int start = insideLine.IndexOf('"') + 1;
+                            int end = insideLine.IndexOf('"', start);
+                            row[1] = insideLine.Substring(start, end - start);
+                        }
+                        if (insideLine.Contains("culture"))
+                        {
+                            // Cultures are added in two ways with "" or without
+                            if (insideLine.Contains('"'))
+                            {
+                                int start = insideLine.IndexOf('"') + 1;
+                                int end = insideLine.IndexOf('"', start);
+                                row[2] = insideLine.Substring(start, end - start);
+                            }
+                            else
+                            {
+                                row[2] = insideLine.Substring(insideLine.IndexOf("=") + 1).Trim();
+                            }
+                        }
+
+                    } while (closingBrackets < openingBrackets);
+
+
+                    // Add the row to our list
+                    rows.Add(row);
+
+                    // Update the progress
+                    progress += 10;
+                    if(progress > 100)
+                        progress = 100;
+
+                    worker.ReportProgress(progress);
+                }               
+            }
+
+            stream.Close();
+            e.Result = rows;
+        }
+
+        void dynastyBackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            toolStripProgressBar.Visible = false;
+
+            List<String[]> rows = e.Result as List<String[]>;
+            foreach (string[] rowArray in rows)
+            {
+                dynastyGridView.Rows.Add(rowArray);
+            }
+
+        }
+
+        void dynastyBackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            toolStripProgressBar.Value = e.ProgressPercentage;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
